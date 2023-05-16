100a82a70803f9214d88b868e884387c95d2905f
emmmmmmmmm <114401144+MineFlyDouYin@users.noreply.github.com>
2023-05-16 18:02:53 +0800

    Add files via upload

---
+++ snapshots/runner.js
@@ -0,0 +1,34 @@
+import path from "path"
+
+const tests = []
+
+export function test(style, name, source, lang) {
+  tests.push({
+    style,
+    name,
+    source,
+    lang: lang || "en",
+  })
+}
+
+export function runTests(r) {
+  return Promise.all(
+    tests.map(tc => {
+      const outputPath = path.join(
+        "snapshots",
+        tc.style,
+        tc.name.replace(/ /g, "-") + ".png",
+      )
+      console.log("running", tc.name)
+      return (async () => {
+        const options = {
+          lang: tc.lang,
+          style: tc.style,
+          scale: 1,
+        }
+        await r.snapshotToFile(tc.source, options, outputPath)
+        console.log("✓ wrote", outputPath)
+      })()
+    }),
+  )
+}
